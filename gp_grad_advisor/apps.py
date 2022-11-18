9e78b09cb6ad22c7dba7ef82b07fa2a1b6400f56
exponentian <exponentian.oh@gmail.com>
2022-11-17 16:02:15 -0800

    deleted grad_advisor

--- gp_grad_advisor/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GpGradAdvisorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gp_program_advisor_director'
--- gp_grad_advisor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'gp_program_advisor_director'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('program_supervision/', views.GetGradSupervision.as_view(), name='get_program_supervision')
-]
